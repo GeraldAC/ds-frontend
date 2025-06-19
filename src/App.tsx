a88ca99db29875d9620441b3f71a2a957440a164
GeraldAC <200332@unsaac.edu.pe>
2025-06-19 10:26:23 -0500

    Inicio: proyecto con Vite + React + TS + Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box textAlign="center" mt={10}>
+      <Heading mb={4}>Hola Chakra UI v2 🚀</Heading>
+      <Button colorScheme="teal">¡Presióname!</Button>
+    </Box>
+  );
+}
+
+export default App;
+
